88888dfe5d578fa6adc5d329e80747bac0614ecf
UdaySimhadrii <tadakaudaysimhadri@gmail.com>
2025-02-17 15:13:24 +0530

    Initial commit

---
+++ days.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ class days{
+ public static void main(String args[]){
+ Scanner Sc = new Scanner(System.in);
+ int X = Sc.nextInt();
+ switch(X){
+ case 1: System.out.println("Sunday");
+ break;
+ case 2: System.out.println("Monday");
+ break;
+ case 3: System.out.println("tuesday");
+ break;
+ case 4: System.out.println("Wedday");
+ break;
+ }
+ }
+ }
